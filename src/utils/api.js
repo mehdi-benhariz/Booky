8eb74e28bd9a964ad8e1fe149b3aac3b1b2ba55c
mehdi-benhariz <benharizmehdi20@gmail.com>
2023-05-16 14:04:56 +0100

    change to hardcoded url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 export const getApiUrl = () => {
     // console.log(process.env.NODE_ENV);
     // console.log(process.env.NEXT_PUBLIC_API_URL_PROD);
-    // return process.env.NODE_ENV === 'production' ?
-    // process.env.NEXT_PUBLIC_API_URL_PROD :
-    // process.env.NEXT_PUBLIC_API_URL_DEV;
-    return process.env.NEXT_PUBLIC_API_URL_PROD;
+    return process.env.NODE_ENV === 'production' ?
+    'https://fake-server-seven.vercel.app' :
+    'http://localhost:3001';
+
 }
