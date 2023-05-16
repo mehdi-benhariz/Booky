8d51893951a5e6ab07d423660b09a3dfd50c7f9a
mehdi-benhariz <benharizmehdi20@gmail.com>
2023-05-16 14:45:57 +0100

    fixed issue of undefined in bookdetails

--- src/pages/books/[id].js
+++ src/pages/books/[id].js
@@ -20,7 +20,7 @@ import { countProgress } from "@/utils/UI";
 
 const BookDetails = ({ book }) => {
   const [newBook, setNewBook] = useState({ ...book });
-  const [newNote, setNewNote] = useState({ book.notes && book.notes[0] });
+  const [newNote, setNewNote] = useState(book.notes && book.notes[0] ? { ...book.notes[0] } : null);
   const router = useRouter();
   const toast = useToast();
 
