a367f780c51b72f2b0c4bad3e00357cc22b3b002
mehdi-benhariz <benharizmehdi20@gmail.com>
2023-05-16 13:23:35 +0100

    check minor issue

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,7 @@
-export const getApiUrl = () => process.env.NODE_ENV === 'production' ?
+export const getApiUrl = () => {
+    console.log(process.env.NODE_ENV);
+    console.log(process.env.NEXT_PUBLIC_API_URL_PROD);
+    return process.env.NODE_ENV === 'production' ?
     process.env.NEXT_PUBLIC_API_URL_PROD :
     process.env.NEXT_PUBLIC_API_URL_DEV;
+}
