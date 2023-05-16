9a7df0cd953be16cf60b554cb4b1fc831bdebfd7
mehdi-benhariz <benharizmehdi20@gmail.com>
2023-05-16 14:14:11 +0100

    fixing a typo

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 export const getApiUrl = () => {
     // console.log(process.env.NODE_ENV);
     // console.log(process.env.NEXT_PUBLIC_API_URL_PROD);
-    return process.env.NODE_ENV === 'production' ?
+    return process.env.NODE_ENV === 'Production' ?
     'https://fake-server-seven.vercel.app' :
     'http://localhost:3001';
 
