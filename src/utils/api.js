69c5b0f308861cac6919133333c7ee09aa549a91
mehdi-benhariz <benharizmehdi20@gmail.com>
2024-02-16 16:46:05 +0100

    changed env var

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,8 @@
 export const getApiUrl = () => {
-    // console.log(process.env.NODE_ENV);
-    // console.log(process.env.NEXT_PUBLIC_API_URL_PROD);
-    // return process.env.NODE_ENV === 'Production' ?
-    // 'https://fake-server-seven.vercel.app' :
-    // 'http://localhost:3001';
-    console.log({ "env url:": process.env.NEXT_PUBLIC_API_URL_PROD });
-    return 'https://fake-server-seven.vercel.app'
+    console.log(process.env.NODE_ENV);
+    console.log(process.env.NEXT_PUBLIC_API_URL_PROD);
+    if (process.env.NODE_ENV === 'Production')
+        return process.env.NEXT_PUBLIC_API_URL_PROD;
+    return 'http://localhost:3001';
+
 }
